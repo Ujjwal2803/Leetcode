150b57be3942e5b89fcc764021c6faad6e967769
Ujjwal-Bharadwaj <114858926+Ujjwal-Bharadwaj@users.noreply.github.com>
2025-01-21 11:49:24 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.55%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& arr, int k) {
-        int start = -1, end = -1;
-        int low = 0, high = arr.size() - 1;
-
-        // Find the first occurrence of k
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] == k) {
-                start = mid;
-                high = mid - 1; // Keep searching in the left half
-            } else if (arr[mid] < k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int m=-1,n=-1;
+        int i=0,j=nums.size()-1;
+        while(i<nums.size() && j>=0){
+        if(nums[i]==target &&  m==-1){
+            m=i;
         }
-
-        low = 0;
-        high = arr.size() - 1;
-
-        // Find the last occurrence of k
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] == k) {
-                end = mid;
-                low = mid + 1; // Keep searching in the right half
-            } else if (arr[mid] < k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+        if(nums[j]==target &&  n==-1){
+            n=j;
         }
-
-        // If k is not found, return {-1, -1}
-        if (start == -1 && end == -1) {
-            return {-1, -1};
+        i++;
+        j--;
         }
-
-        return {start, end};
+        return {m,n};
     }
 };
