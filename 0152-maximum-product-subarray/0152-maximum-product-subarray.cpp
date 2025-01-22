d9dea86af2df7f7bcaaa76f40f1d91fe0628713c
Ujjwal-Bharadwaj <114858926+Ujjwal-Bharadwaj@users.noreply.github.com>
2025-01-22 18:27:51 +0530

    Time: 0 ms (100%), Space: 17.7 MB (78%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,58 @@
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+    //    int sum=1;
+    //     int maxn=INT_MIN;
+    //     if(arr.size()==1){
+    //         return arr[0];
+    //     }
+    //     int i=0,j=1;
+    //     for(int k=i;k<=j;k++){
+    //         sum*=(arr[k]);
+    //     }
+    //     maxn=max(maxn,(sum));
+    //     j++;
+    //     while(j<=arr.size()-1){
+    //         sum=sum*(arr[j++])/(arr[i++]);
+    //         maxn=max(maxn,(sum));
+    //     }
+    //     return maxn;
+//     long prod=1;
+//     long long max=nums[0];
+//     for(int i=0;i<nums.size();i++){
+//         if(prod==0 || prod<INT_MIN)prod=1;
+//         prod*=nums[i];
+//         if(prod>max) max=prod;
+//     }
+//     prod=-1;
+//     for(int i=nums.size()-1;i>=0;i--){
+//         if(prod==0 || prod<INT_MIN)prod=1;
+//         prod*=nums[i];
+//         if(prod>max) max=prod;
+//     }
+//     return max;
+//     } 
+    
+// };
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        // Initialize variables
+        long long maxProduct = nums[0]; // To store the maximum product
+        long long currentProduct = 1;  // For forward traversal
+        long long reverseProduct = 1;  // For backward traversal
+        for (int i = 0; i < nums.size(); i++) {
+            if (currentProduct == 0 || currentProduct < INT_MIN) currentProduct = 1;
+            currentProduct *= nums[i];
+            maxProduct = max(maxProduct, currentProduct);
+        }
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (reverseProduct == 0 || reverseProduct < INT_MIN) reverseProduct = 1;
+            reverseProduct *= nums[i];
+            maxProduct = max(maxProduct, reverseProduct);
+        }
+        return maxProduct;
+    }
+};
