4faf0d103d420324fca3fc350a568abc35e6e681
Ujjwal-Bharadwaj <114858926+Ujjwal-Bharadwaj@users.noreply.github.com>
2025-01-22 18:29:55 +0530

    Time: 0 ms (100%), Space: 17.8 MB (52.87%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,6 +1,6 @@
-// class Solution {
-// public:
-//     int maxProduct(vector<int>& nums) {
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
     //    int sum=1;
     //     int maxn=INT_MIN;
     //     if(arr.size()==1){
@@ -17,42 +17,42 @@
     //         maxn=max(maxn,(sum));
     //     }
     //     return maxn;
-//     long prod=1;
-//     long long max=nums[0];
-//     for(int i=0;i<nums.size();i++){
-//         if(prod==0 || prod<INT_MIN)prod=1;
-//         prod*=nums[i];
-//         if(prod>max) max=prod;
-//     }
-//     prod=-1;
-//     for(int i=nums.size()-1;i>=0;i--){
-//         if(prod==0 || prod<INT_MIN)prod=1;
-//         prod*=nums[i];
-//         if(prod>max) max=prod;
-//     }
-//     return max;
-//     } 
-    
-// };
-
-
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        // Initialize variables
-        long long maxProduct = nums[0]; // To store the maximum product
-        long long currentProduct = 1;  // For forward traversal
-        long long reverseProduct = 1;  // For backward traversal
-        for (int i = 0; i < nums.size(); i++) {
-            if (currentProduct == 0 || currentProduct < INT_MIN) currentProduct = 1;
-            currentProduct *= nums[i];
-            maxProduct = max(maxProduct, currentProduct);
-        }
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (reverseProduct == 0 || reverseProduct < INT_MIN) reverseProduct = 1;
-            reverseProduct *= nums[i];
-            maxProduct = max(maxProduct, reverseProduct);
-        }
-        return maxProduct;
+    long long prod=1;
+    long long max=nums[0];
+    for(int i=0;i<nums.size();i++){
+        if(prod==0 || prod<INT_MIN)prod=1;
+        prod*=nums[i];
+        if(prod>max) max=prod;
     }
+    prod=1;
+    for(int i=nums.size()-1;i>=0;i--){
+        if(prod==0 || prod<INT_MIN)prod=1;
+        prod*=nums[i];
+        if(prod>max) max=prod;
+    }
+    return max;
+    } 
+    
 };
+
+
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         // Initialize variables
+//         long long maxProduct = nums[0]; // To store the maximum product
+//         long long currentProduct = 1;  // For forward traversal
+//         long long reverseProduct = 1;  // For backward traversal
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (currentProduct == 0 || currentProduct < INT_MIN) currentProduct = 1;
+//             currentProduct *= nums[i];
+//             maxProduct = max(maxProduct, currentProduct);
+//         }
+//         for (int i = nums.size() - 1; i >= 0; i--) {
+//             if (reverseProduct == 0 || reverseProduct < INT_MIN) reverseProduct = 1;
+//             reverseProduct *= nums[i];
+//             maxProduct = max(maxProduct, reverseProduct);
+//         }
+//         return maxProduct;
+//     }
+// };
