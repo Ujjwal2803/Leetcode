5c7a2512b5eba0033998ec25279f0c2a1cd82ef1
Ujjwal-Bharadwaj <114858926+Ujjwal-Bharadwaj@users.noreply.github.com>
2025-01-24 10:34:43 +0530

    Time: 24 ms (11.5%), Space: 10.9 MB (71.47%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.size(),ans=0;
+        for(char c='A'; c<='Z';c++){
+            int i=0,j=0,count=0;
+            while(j<n){
+                if(s[j]!=c){
+                    count++;
+                }
+                while(count>k){
+                    if(s[i]!=c)count--;
+                    i++;
+                }
+                ans=max(ans,j-i+1);
+                j++;
+            }
+            ans=max(ans,j-i);
+        }
+        return ans;
+    }
+};
