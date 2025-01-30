d4067784da25c74d15b090411de5007c2da57dda
Ujjwal Bharadwaj <114858926+Ujjwal2803@users.noreply.github.com>
2025-01-30 09:44:08 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.28%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int k) {
-        int low = 0, high =arr.size() - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        //if mid points the target
-        if (arr[mid] == k) return mid;
-        //if left part is sorted:
-        if (arr[low] <= arr[mid]) {
-            if (arr[low] <= k && k <= arr[mid]) {
-                //element exists:
-                high = mid - 1;
+    int search(vector<int>& arr, int target) {
+        int low=0,high=arr.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target==arr[mid]){
+                return mid;
             }
-            else {
-                //element does not exist:
-                low = mid + 1;
+            if(arr[low]<=arr[mid]){
+                if(arr[low] <=target && arr[mid]>=target){//left part sorted
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(arr[mid]<=target && arr[high]>=target){//right part sorted
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
             }
+
         }
-        else { //if right part is sorted:
-            if (arr[mid] <= k && k <= arr[high]) {
-                //element exists:
-                low = mid + 1;
-            }
-            else {
-                //element does not exist:
-                high = mid - 1;
-            }
-        }
-    }
-    return -1;
+        return -1;
     }
 };
