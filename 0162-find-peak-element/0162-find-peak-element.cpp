2a06285cbe1e8624a1fe8cf74ec0befd9c5c749f
Ujjwal Bharadwaj <114858926+Ujjwal2803@users.noreply.github.com>
2025-01-30 21:42:05 +0530

    Time: 0 ms (100%), Space: 12.4 MB (65.24%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& arr) {
-        int n = arr.size();
-        if (n == 1) return 0; // Single element, it is the peak
-
-        // Handle the edge cases where the first or last element is the peak
+        int n = arr.size(); //Size of array.
+        if (n == 1) return 0;
         if (arr[0] > arr[1]) return 0;
         if (arr[n - 1] > arr[n - 2]) return n - 1;
 
-        // Iterate through the array to find a peak element
-        for (int i = 1; i <= n - 2; i++) {
-            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
-                return i; // Return the index of the peak
-            }
-        }
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+            return mid;
+        if (arr[mid] > arr[mid - 1]) low = mid + 1;
+        else high = mid - 1;
+    }
 
-        return -1; // This line should not be reached
+    return -1; 
     }
 };
