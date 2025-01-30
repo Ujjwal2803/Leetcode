3251976e7ace6dda307397969e031fcb4414049b
Ujjwal Bharadwaj <114858926+Ujjwal2803@users.noreply.github.com>
2025-01-30 10:50:58 +0530

    Time: 0 ms (100%), Space: 17.8 MB (89.6%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,31 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        set<int>p(nums.begin(),nums.end());
-        if(p.find(target)!=p.end()){
-            return true;
+    bool search(vector<int>& arr, int target) {
+        int low=0,high=arr.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target==arr[mid]){
+                return true;
+            }
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low++;
+                high--;
+                continue;
+            }
+            if(arr[low]<=arr[mid]){
+                if(arr[low] <=target && arr[mid]>target){//left part sorted
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(arr[mid]<target && arr[high]>=target){//right part sorted
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+
         }
         return false;
     }
