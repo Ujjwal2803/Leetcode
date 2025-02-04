d657056763134dfdf668693c358d58983ce7e8e4
Ujjwal Bharadwaj <114858926+Ujjwal2803@users.noreply.github.com>
2025-02-04 19:28:13 +0530

    Time: 0 ms (100%), Space: 22.2 MB (17.51%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,31 +2,8 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int>s;
-        // vector<int>q;
-        // for(int i=0;i<asteroids.size();i++){
-            // while(p.size() && ((asteroids[i]<0 && p.top()>0) || (asteroids[i]>0 && p.top()<0))){
-            //     if(abs(p.top())==abs(asteroids[i])){
-            //         p.pop();
-            //         continue;
-            //     }
-            //     // else if(abs(p.top())>abs(asteroids[i])){
-            //     //     q.push_back(max(abs(p.top()),abs(asteroids[i])));
-            //     // }
-            //     else{
-            //         q.push_back(max(abs(p.top()),abs(asteroids[i])));
-            //         p.pop();
-            //         p.push(max(abs(p.top()),abs(asteroids[i])));
-            //     }
-            // }
-            // // if((asteroids[i]<0 && p.top()<0) || (asteroids[i]>0 && p.top()>0)){
-            // //     q.push_back(p.top());
-            // // }
-            // p.push(asteroids[i]);
-        // }
-        // return q;
         for (int asteroid : asteroids) {
             bool destroyed = false;
-            
             while (!s.empty() && asteroid < 0 && s.top() > 0) {
                 if (abs(s.top()) < abs(asteroid)) {
                     s.pop();  // Destroy the top asteroid
